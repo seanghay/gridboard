b73ad8ab09120d6023aa803e2af84cba0f8ffe4f
Yath Seanghay <seanghay.dev@gmail.com>
2022-02-24 14:03:38 +0700

    chore: add test & refactored package names

---
+++ packages/web/src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import KTextField from "./components/KTextField.vue";
+import KButton from "./components/KButton.vue";
+import debounce from "lodash/debounce";
+import {
+  computed,
+  ref,
+  onMounted,
+  onBeforeUnmount,
+  unref,
+  watch,
+  reactive,
+} from "vue";
+import type { Ref, ComputedRef } from "vue";
+</script>
+
+<template>
+  <k-text-field class="m-4" placeholder="Grid"></k-text-field>
+</template>
+
+
+<style>
+.canvas {
+  transition: 0.1s ease;
+  overflow: hidden;
+}
+</style>
